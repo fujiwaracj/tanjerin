a4210ee6969f5d2ef09e9c8a286865f4fff57c12
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-02-25 22:13:19 +0800

    Add analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata, Viewport } from "next";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -20,7 +22,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-orange-50 text-orange-950 h-dvh`}>{children}</body>
+      <body className={`${inter.className} bg-orange-50 text-orange-950 h-dvh`}>
+        {children}
+        <Analytics />
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
