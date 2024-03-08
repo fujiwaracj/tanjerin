ca65435c042bba7898b85e76ee4e187eb509610b
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-03-08 19:28:58 +0800

    Fix styles on mobile

--- app/chat.tsx
+++ app/chat.tsx
@@ -36,7 +36,7 @@ export function Chat() {
         return messages.map(m => (
             <div className='my-4' key={m.id}>
                 <div className='flex flex-col lg:flex-row'>
-                    <div className='mx-2 p-2.5 rounded-full bg-orange-100 size-12 flex items-center justify-center'>
+                    <div className='lg:mx-2 my-2 lg:my-0 p-2.5 rounded-full bg-orange-100 size-12 flex items-center justify-center'>
                         {m.role === 'user' ? <User className='text-orange-900 size-8' /> : <Citrus className='text-orange-900 size-8' />}
                     </div>
                     <div className='basis-full bg-orange-50 rounded-lg px-4 '>
@@ -52,17 +52,17 @@ export function Chat() {
     return (
         <div className="mx-auto flex h-full flex-col md:h-fit lg:max-w-screen-xl lg:px-8">
             <div className="h-full gap-x-4 md:h-fit lg:flex-row">
-                <div className="z-10 mx-auto flex h-full flex-col justify-between rounded-lg border p-2 px-6 shadow md:mt-4 md:h-fit lg:max-w-screen-sm">
+                <div className="z-10 mx-auto flex h-full flex-col justify-between lg:rounded-lg  lg:py-2 lg:shadow md:mt-4 md:h-fit lg:max-w-screen-sm">
                     <div className="flex max-h-[60svh] flex-col">
-                        <div className="my-4 min-h-32 overflow-y-auto rounded-lg p-4  md:max-w-screen-sm">
+                        <div className="min-h-32 overflow-y-auto rounded-lg p-4  md:max-w-screen-sm">
                             <div className='prose prose-orange lg:prose-xl'>
                                 <Result />
                             </div>
                         </div>
                     </div>
-                    <form onSubmit={handleSubmit} className="grid gap-y-4">
+                    <form onSubmit={handleSubmit} className="grid gap-y-4 border-t px-4 border-orange-200 py-2">
                         <textarea
-                            className="inline-flex min-h-48 rounded-md border border-orange-800 px-3 py-1.5 text-xl font-medium text-orange-800 outline-2 outline-offset-2 placeholder:text-orange-800/50 focus-visible:outline-2 focus-visible:outline-orange-500"
+                            className="inline-flex min-h-48 rounded-md border border-orange-200 px-3 py-1.5 text-xl font-medium text-orange-800 outline-2 outline-offset-2 placeholder:text-orange-800/50 focus-visible:outline-orange-200"
                             value={input}
                             placeholder="What do you want to cook?"
                             required
@@ -73,14 +73,14 @@ export function Chat() {
                             <button
                                 type="button"
                                 onClick={stop}
-                                className="h-9 rounded-md border border-orange-800 px-3  py-1.5 font-medium text-orange-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-500"
+                                className="h-9 rounded-md border border-orange-200 px-3  py-1.5 font-medium text-orange-900 focus-visible:outline focus-visible:outline-orange-200"
                             >
                                 Stop
                             </button>
                         ) : (
                             <button
                                 type="submit"
-                                className="h-9 rounded-md border border-orange-800 px-3  py-1.5 font-medium text-orange-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-orange-500"
+                                className="h-9 rounded-md border bg-orange-100 border-orange-200 px-3  py-1.5 font-medium text-orange-900 focus-visible:outline focus-visible:outline-orange-200"
                             >
                                 Submit
                             </button>
