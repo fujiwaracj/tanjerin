26439ef5f4fcbdc9948979b1e1bb608af2508231
John Carlo Austria <cpe.austria.johncarlo@gmail.com>
2024-03-14 22:38:02 +0800

    Mobile redesign and optimization to markdown rendering

--- app/chat.tsx
+++ app/chat.tsx
@@ -1,84 +1,184 @@
 'use client'
 
-import { useChat } from 'ai/react'
-import { Remark } from 'react-remark'
-import { User, Citrus } from 'lucide-react'
+import { Message, useChat } from 'ai/react'
+import ReactMarkdown, { Options } from 'react-markdown'
+import { User, Citrus, SendIcon, StopCircleIcon, LightbulbIcon } from 'lucide-react'
+import { FC, memo, useEffect, useState } from 'react'
+import { useInView } from 'react-intersection-observer'
+import Textarea from 'react-textarea-autosize'
 
-export function Chat() {
-    const {
-        messages,
-        input,
-        handleInputChange,
-        handleSubmit,
-        isLoading,
-        stop,
-    } = useChat({
-        initialInput:
-            'Hello! Its currently 5:00PM and I wanted to create a simple meal for my dinner so that I can go ahead and sleep later, make sure it is light and simple enough to make',
-    })
+export function useAtBottom(offset = 0) {
+    const [isAtBottom, setIsAtBottom] = useState(false)
 
-    const Result = () => {
-        if (messages.length === 0) {
-            return (
-                <p className="text-center font-semibold text-orange-800/80">
-                    Generate your first meal recipe by entering a prompt
-                </p>
+    useEffect(() => {
+        const handleScroll = () => {
+            setIsAtBottom(
+                window.innerHeight + window.scrollY >=
+                document.body.offsetHeight - offset
             )
         }
 
-        return messages.map(m => (
-            <div className='my-4' key={m.id}>
-                <div className='flex flex-col lg:flex-row'>
-                    <div className='lg:mx-2 my-2 lg:my-0 p-2.5 rounded-full bg-orange-100 size-12 flex items-center justify-center'>
-                        {m.role === 'user' ? <User className='text-orange-900 size-8' /> : <Citrus className='text-orange-900 size-8' />}
+        window.addEventListener('scroll', handleScroll, { passive: true })
+        handleScroll()
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll)
+        }
+    }, [offset])
+
+    return isAtBottom
+}
+
+const MemoizedReactMarkdown: FC<Options> = memo(ReactMarkdown, (prevProps, nextProps) => prevProps.children === nextProps.children && prevProps.className === nextProps.className)
+
+const EmptyScreen = ({ defaultValues, setInput, }: { defaultValues: string[], setInput: (input: string) => void }) => {
+    return (
+        <>
+            <strong className="font-semibold text-[#582D1D]">
+                Generate your first meal recipe by entering a prompt
+            </strong>
+            <p className='text-[#582D1D] my-4'>
+                I can help you create you cook your next meal,
+                just say what you want and I&apos;ll teach you how to make it!
+            </p>
+            <p className='text-[#582D1D]'>
+                Here are a few examples for you to get started:
+            </p>
+            <div className='flex gap-4 py-4 px-2 overflow-x-scroll'>
+                <button onClick={() => setInput(defaultValues[0])}>
+
+                    <div className='min-w-[192px] min-h-[192px] bg-[#FFDFB5] border-[#FFC182] border rounded-lg px-4 py-2'>
+                        <div className="w-[40px] h-[40px] rounded-full inline-flex justify-center items-center border-[#FFC182] bg-[#FFEFD6] border shadow">
+                            <LightbulbIcon />
+                        </div>
+                        <p>
+                            {defaultValues[0]}
+                        </p>
                     </div>
-                    <div className='basis-full bg-orange-50 rounded-lg px-4 '>
-                        <Remark>
-                            {m.content}
-                        </Remark>
+                </button>
+                <button onClick={() => setInput(defaultValues[1])}>
+                    <div className='min-w-[192px] min-h-[192px] bg-[#FFDFB5] border-[#FFC182] border rounded-lg px-4 py-2'>
+                        <div className="w-[40px] h-[40px] rounded-full inline-flex justify-center items-center border-[#FFC182] bg-[#FFEFD6] border shadow">
+                            <LightbulbIcon />
+                        </div>
+                        <p>
+                            {defaultValues[1]}
+
+                        </p>
+                    </div>
+                </button>
+                <button onClick={() => setInput(defaultValues[2])}>
+                    <div className='min-w-[192px] min-h-[192px] bg-[#FFDFB5] border-[#FFC182] border rounded-lg px-4 py-2'>
+                        <div className="w-[40px] h-[40px] rounded-full inline-flex justify-center items-center border-[#FFC182] bg-[#FFEFD6] border shadow">
+                            <LightbulbIcon />
+                        </div>
+                        <p>
+                            {defaultValues[2]}
+                        </p>
+                    </div>
+                </button>
+
+            </div>
+        </>
+    )
+}
+
+const ChatMessages = ({ messages, setInput, defaultValues }: { messages: Message[], setInput: (input: string) => void, defaultValues: string[] }) => {
+    return (
+        <div className='relative mx-auto max-w-2xl px-4'>
+            {messages.length ? messages.map(m => (
+                <div className='my-4' key={m.id}>
+                    <div className='flex flex-col lg:flex-row'>
+                        <div className='lg:mx-2 my-2 lg:my-0 p-2.5 rounded-full bg-orange-100 size-12 flex items-center justify-center'>
+                            {m.role === 'user' ? <User className='text-orange-900 size-8' /> : <Citrus className='text-orange-900 size-8' />}
+                        </div>
+                        <div className='basis-full bg-orange-50 rounded-lg px-4 '>
+                            <MemoizedReactMarkdown>
+                                {m.content}
+                            </MemoizedReactMarkdown>
+                        </div>
                     </div>
                 </div>
-            </div>
-        ))
-    }
+            )) : (<EmptyScreen defaultValues={defaultValues} setInput={setInput} />)}
+        </div>)
+}
+
+const PromptForm = ({ handleSubmit, input, handleInputChange, isLoading }: { handleSubmit: React.FormEventHandler, input: string, setInput: (input: string) => void, isLoading: boolean, handleInputChange: React.ChangeEventHandler<HTMLTextAreaElement> }) => {
+    return (
+        <div className="fixed bottom-0 inset-x-0">
+            <form onSubmit={handleSubmit} className='backdrop-blur flex items-center justify-between border min-w-[284px] border-[#FFC182] bg-[#FFDFB5]/50 rounded-lg px-2 mx-4 my-4 focus:outline'>
+                <Textarea
+                    className="inline-flex flex-1 py-[0.85rem] px-4 rounded-md align-middle font-medium bg-transparent text-[#CC4E00] placeholder:text-[#CC4E00]/50 outline-none"
+                    placeholder="Send a message."
+                    required
+                    onChange={handleInputChange}
+                    value={input}
+                    disabled={isLoading}
+                />
+                {isLoading ? (
+                    <button
+                        type="button"
+                        onClick={stop}
+                        className="inline-flex justify-center items-center p-1 size-9 rounded-md bg-[#FFDFB5] font-medium text-orange-900 border border-[#FFC182] focus-visible:outline focus-visible:outline-orange-200"
+                    >
+                        <StopCircleIcon />
+                    </button>
+                ) : (
+                    <button
+                        type="submit"
+                        className="inline-flex justify-center items-center p-1 size-9 rounded-md bg-[#FFDFB5] font-medium text-orange-900 border border-[#FFC182] focus-visible:outline focus-visible:outline-orange-200"
+                        disabled={input.length === 0}
+                    >
+                        <SendIcon />
+                    </button>
+                )}
+            </form>
+        </div>
+    )
+}
+
+export function Chat() {
+    const {
+        input,
+        messages,
+        handleSubmit,
+        handleInputChange,
+        isLoading,
+        stop,
+        setInput,
+    } = useChat()
+    const { ref, entry, inView } = useInView({
+        trackVisibility: isLoading,
+        delay: 100,
+    })
+    const isAtBottom = useAtBottom()
+    const defaultValues = [
+        'Develop a hearty and comforting stew inspired by French cuisine.',
+        "Create a one-pot Thai dish that's perfect for a weeknight meal.",
+        'Design a vegetarian appetizer platter with global influences.'
+    ]
+
+    useEffect(() => {
+        if (isAtBottom && isLoading && !inView) {
+            entry?.target.scrollIntoView({
+                block: 'start'
+            })
+        }
+    }, [])
 
     return (
         <div className="mx-auto flex h-full flex-col md:h-fit lg:max-w-screen-xl lg:px-8">
             <div className="h-full gap-x-4 md:h-fit lg:flex-row">
                 <div className="z-10 mx-auto flex h-full flex-col justify-between lg:rounded-lg  lg:py-2 lg:shadow md:mt-4 md:h-fit lg:max-w-screen-sm">
-                    <div className="flex max-h-[60svh] flex-col">
+                    <main className="flex max-h-full flex-col">
                         <div className="min-h-32 overflow-y-auto rounded-lg p-4  md:max-w-screen-sm">
-                            <div className='prose prose-orange lg:prose-xl'>
-                                <Result />
+                            <div>
+                                <ChatMessages defaultValues={defaultValues} messages={messages} setInput={setInput} />
+                                <div ref={ref} className='h-px w-full' />
                             </div>
                         </div>
-                    </div>
-                    <form onSubmit={handleSubmit} className="grid gap-y-4 border-t px-4 border-orange-200 py-2">
-                        <textarea
-                            className="inline-flex min-h-48 rounded-md border border-orange-200 px-3 py-1.5 text-xl font-medium text-orange-800 outline-2 outline-offset-2 placeholder:text-orange-800/50 focus-visible:outline-orange-200"
-                            value={input}
-                            placeholder="What do you want to cook?"
-                            required
-                            disabled={isLoading}
-                            onChange={handleInputChange}
-                        />
-                        {isLoading ? (
-                            <button
-                                type="button"
-                                onClick={stop}
-                                className="h-9 rounded-md border border-orange-200 px-3  py-1.5 font-medium text-orange-900 focus-visible:outline focus-visible:outline-orange-200"
-                            >
-                                Stop
-                            </button>
-                        ) : (
-                            <button
-                                type="submit"
-                                className="h-9 rounded-md border bg-orange-100 border-orange-200 px-3  py-1.5 font-medium text-orange-900 focus-visible:outline focus-visible:outline-orange-200"
-                            >
-                                Submit
-                            </button>
-                        )}
-                    </form>
+                    </main>
+                    <PromptForm handleSubmit={handleSubmit} input={input} setInput={setInput} isLoading={isLoading} handleInputChange={handleInputChange} />
                 </div>
             </div>
         </div>
